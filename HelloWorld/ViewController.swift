bcdb88a0200950984bf9dd1c2952debce31946c6
Ekaterina VAKHROMEEVA <Ekaterina.VAKHROMEEVA@raiffeisen.ru>
2019-08-28 09:04:53 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by VAKHROMEEVA Ekaterina on 28/08/2019.
+//  Copyright © 2019 VAKHROMEEVA Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
